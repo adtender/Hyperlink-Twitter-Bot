0bf4ea56adad06036025884dea0d2dceca5f86e5
Brandon DeYoung <deyoung.brandon@gmail.com>
2022-05-13 21:49:31 -0400

    lambda function

--- config.py
+++ config.py
@@ -2,7 +2,12 @@ API_Key = ''
 API_Key_Secret = ''
 Access_Token = ''
 Access_Token_Secret = ''
+# API Key and access token found when creating twitter developer program
+# found here: https://developer.twitter.com/
+
+account = ''
+# @ of the account, ex: 'RNGundam'
 
 videoSource = ''
 # in the format of 'https://twitter.com/acount/status/###################/video/1'
-# Can be acquired from right clicking the timecode of a tweet and selecting copy link address, will need /video/1 appended 
+# Can be acquired from right clicking the timecode of a tweet and selecting copy link address, will need /video/1 appended
